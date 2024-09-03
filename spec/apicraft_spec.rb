b70730447feeae52505a9fd3c8b8c4ec056fde5a
Abhishek Sarkar <abhisheksarka@gmail.com>
2024-09-03 23:20:23 +0530

    Rspec initial setup and integration

--- spec/apicraft_spec.rb
+++ spec/apicraft_spec.rb
@@ -1,7 +1,28 @@
 # frozen_string_literal: true
 
+require "spec_helper"
+
 RSpec.describe Apicraft do
   it "has a version number" do
     expect(Apicraft::VERSION).not_to be_nil
+  end
+
+  describe '.configure' do
+    it 'yields the shared config object' do
+      expect { |b| Apicraft.configure(&b) }.to yield_with_args(Apicraft.config)
+    end
+
+    it 'allows configuration via a block' do
+      Apicraft.configure do |config|
+        config.dir = '/test'
+        config.mocks = false
+        config.strict_reference_validation = false
+      end
+
+      c = Apicraft.config
+      expect(c.dir).to eq('/test')
+      expect(c.mocks).to eq(false)
+      expect(c.strict_reference_validation).to eq(false)
+    end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
-require "apicraft"
+require "pry"
+require_relative "../lib/apicraft-rails"
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
